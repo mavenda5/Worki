8dcfd014b03bc86cf0eb452d2e796f8f76b84dd6
anderson <agomezquint@uniminuto.edu.co>
2021-11-25 19:51:55 -0500

    HOME actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+
+import { Component,OnInit,DoCheck } from '@angular/core';
+import { Router,ActivatedRoute } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 import { LoginComponent } from './components/login/login.component';
+import { ApiService } from './servicio/home.service';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +11,10 @@ import { LoginComponent } from './components/login/login.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   public title = 'WORKI - SOCIAL NETWORK';
   description = 'Benvenido a su nueva red social';
 }
+
 
 
